ca000137435d7e434ca13b1c796768fb5ae94565
Dirofilaria-immitis <73492731+Dirofilaria-immitis@users.noreply.github.com>
2021-01-11 23:16:08 +0900

    Update news_test.php

--- news_test.php
+++ news_test.php
@@ -1,9 +1,9 @@
 <?php
   $data = '[
-    {"id":"1", "name":"Vasdads", "email":"afaf@masfa.com"},
-    {"id":"2", "name":"Gdsfs", "email":"sdafasdaf@msdfafa.com"},
-    {"id":"3", "name":"Hfdsg", "email":"fbfzdff@wqghreh.com"}
-    ]';
+     {"id":"1", "name":"Vasdads", "email":"afaf@masfa.com"},
+     {"id":"2", "name":"Gdsfs", "email":"sdafasdaf@msdfafa.com"},
+     {"id":"3", "name":"Hfdsg", "email":"fbfzdff@wqghreh.com"}
+  ]';
 
-    echo $_GET["callback"]."(".$data.")";
+  echo $_GET["callback"]."(".$data.")";
  ?>
