5824ad300786d09d4d2bf24c184431241c7120e9
Dirofilaria-immitis <73492731+Dirofilaria-immitis@users.noreply.github.com>
2021-03-29 01:22:19 +0900

    깃허브 설치 기념 리뉴열

--- news_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $data = '[
-     {"id":"1", "name":"Vasdads", "email":"afaf@masfa.com"},
-     {"id":"2", "name":"Gdsfs", "email":"sdafasdaf@msdfafa.com"},
-     {"id":"3", "name":"Hfdsg", "email":"fbfzdff@wqghreh.com"}
-  ]';
-
-  echo $_GET["callback"]."(".$data.")";
- ?>
